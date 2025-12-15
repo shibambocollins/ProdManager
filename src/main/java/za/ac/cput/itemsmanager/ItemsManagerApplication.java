ce1f9d22a3ff51bb88b3ff56222f8681bec42ce2
shibambocollins <ntsobokwanec@gmail.com>
2025-12-15 14:17:57 +0200

    initial commit

---
+++ src/main/java/za/ac/cput/itemsmanager/ItemsManagerApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.itemsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/cput/itemsmanager/ItemsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.itemsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
