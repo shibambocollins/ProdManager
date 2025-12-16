08d2c313a5c2208b7890970ac833ccadca7f0b5b
shibambocollins <ntsobokwanec@gmail.com>
2025-12-16 22:22:19 +0200

    added entity class with it's attributes

--- src/main/java/za/ac/cput/itemsmanager/entity/Product.java
+++ src/main/java/za/ac/cput/itemsmanager/entity/Product.java
@@ -1,4 +1,25 @@
 package za.ac.cput.itemsmanager.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(length = 500)
+    private String description;
 }
