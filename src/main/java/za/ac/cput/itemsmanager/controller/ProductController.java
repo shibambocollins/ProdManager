aba2168b33852a23618b1a6eba48f3fdb5a268e8
shibambocollins <ntsobokwanec@gmail.com>
2025-12-19 13:07:40 +0200

    remove cross origin annotation

--- src/main/java/za/ac/cput/itemsmanager/controller/ProductController.java
+++ src/main/java/za/ac/cput/itemsmanager/controller/ProductController.java
@@ -12,7 +12,6 @@ import java.util.List;
     @RestController
     @RequestMapping("/api/products")
     @RequiredArgsConstructor
-    @CrossOrigin(origins = "http://localhost:*")
     public class ProductController {
 
         private final ProductService productService;
